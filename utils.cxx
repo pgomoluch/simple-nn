7a3a87a5ae8268352465e4173d539a1c06197163
Pawel Gomoluch <pawel.gomoluch14@imperial.ac.uk>
2017-06-14 15:29:20 +0100

    Automatic detection of the number of features.

--- utils.cxx
+++ utils.cxx
@@ -2,21 +2,29 @@
 
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
 
 using namespace std;
-
-const int N_FEATURES = 5; 
 
 bool read_data(const char *feature_path, const char *label_path, std::vector<std::vector<double> > &features, std::vector<double> &labels)
 {
     ifstream feature_file(feature_path);
     
+    string first_line;
+    getline(feature_file, first_line);
+    feature_file.seekg(0);
+    stringstream first_line_stream(first_line);
+    int n_features = 0;
     double d;
+    while(first_line_stream >> d)
+        ++n_features;
+    
     vector<double> sample;
     while(feature_file >> d)
     {
         sample.push_back(d);
-        if(sample.size() == N_FEATURES)
+        if(sample.size() == n_features)
         {
             features.push_back(sample);
             sample.clear();
