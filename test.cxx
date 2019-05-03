7e1c705aa7d22394dbdb556a42f1ce2548de3829
Pawel Gomoluch <pawel.gomoluch14@imperial.ac.uk>
2019-05-03 12:29:11 +0100

    Some test functions.

---
+++ test.cxx
@@ -0,0 +1,113 @@
+#include "matrix.h"
+#include "legacy/network.h"
+#include "network.h"
+
+#include <chrono>
+#include <iostream>
+
+using namespace std;
+
+void matrix_test()
+{
+    cout << "Matrix class test" << endl;
+    
+    double a[] = {
+        1.0, 2.0,
+        3.0, 4.0
+    };
+    double b[] = {
+        -1.0, 0.0,
+        0.0, 0.5
+    };
+
+    Matrix m1(2, 2, a);
+    cout << m1;
+    Matrix m2(2, 2, {1.0, 2.0, 3.0});
+    cout << m2;
+    
+    Matrix m3(2, 2, b);
+    Matrix m4(2, 2);
+    
+    Matrix::multiply(m1, m3, m4);
+    cout << m4;
+    
+    Matrix::point_multiply(m1,m2,m4);
+    cout << m4;
+
+    Matrix m5(2, 3, {1,2,3,4,5,6});
+    cout << "m5\n" << m5 << "m5^T\n" << m5.T();
+    cout << endl;
+}
+
+// Assumes there's a network file 'network.txt', defining a network with two inputs.
+void forward_pass_test()
+{
+    cout << "Forward pass comparison" << endl;
+    
+    legacy::Network n1("network.txt");
+    Network n2("network.txt");
+    
+    const vector<double> sample = {1.0, 2.0};
+    cout << "Old: " << n1.evaluate(sample) << "\n";
+    cout << "New: " << n2.evaluate(sample) << "\n";
+}
+
+// Assumes there's a network file 'network.txt', defining a network with two inputs.
+// Will create 'network-t1.txt' and 'network-t2.txt', which should be the same.
+void train_test()
+{
+    cout << "Training comparison" << endl;
+    
+    legacy::Network n1("network.txt");
+    Network n2("network.txt");
+
+    const vector<vector<double>> features = {{1.0, 2.0}};
+    const vector<double> labels = {1.0};
+
+    chrono::high_resolution_clock::time_point t1, t2;
+
+    t1 = chrono::high_resolution_clock::now();
+    n1.train(features, labels, 100000, 0.0001);
+    t2 = chrono::high_resolution_clock::now();
+    cout << "Old network train time: " << chrono::duration_cast<chrono::milliseconds>(t2-t1).count() << "ms" << endl;
+    n1.save("network-t1.txt");
+
+    t1 = chrono::high_resolution_clock::now();
+    n2.train(features, labels, 100000, 0.0001);
+    t2 = chrono::high_resolution_clock::now();
+    cout << "New network train time: " << chrono::duration_cast<chrono::milliseconds>(t2-t1).count() << "ms" << endl;
+    n2.save("network-t2.txt");
+
+    double result;
+
+    t1 = chrono::high_resolution_clock::now();
+    result = n1.evaluate(features[0]);
+    t2 = chrono::high_resolution_clock::now();
+    cout << "Old result: " << result << endl;
+    cout << "Old network evaluation time: " << chrono::duration_cast<chrono::nanoseconds>(t2-t1).count() << "ns" << endl;
+
+    t1 = chrono::high_resolution_clock::now();
+    result = n2.evaluate(features[0]);
+    t2 = chrono::high_resolution_clock::now();
+    cout << "New result: " << result << endl;
+    cout << "New network evaluation time: " << chrono::duration_cast<chrono::nanoseconds>(t2-t1).count() << "ns" << endl;
+}
+
+// Will create 'network_constructor_test.txt'
+void network_constructor_test()
+{
+    cout << "Network constructor test";
+    vector<unsigned> shape({3,4,2});
+    legacy::Network nn2(shape);
+    nn2.save("network_constructor_test.txt");
+}
+
+int main()
+{
+    matrix_test();
+    forward_pass_test();
+    train_test();
+    network_constructor_test();
+    
+    return 0;
+}
